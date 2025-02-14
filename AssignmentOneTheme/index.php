aa3fea21e28b3de7d85501a3531ff9884c72ad7b
Kevin <kevinrodk@hotmail.com>
2025-02-14 17:21:02 -0500

    Add files via upload

---
+++ AssignmentOneTheme/index.php
@@ -0,0 +1,18 @@
+<?php
+get_header();
+$featuredImg = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
+?>
+
+<section class="post-masthead" style="background: url('<?php echo $featuredImg[0]; ?>')">
+    <div>
+        <h1><?php the_title(); ?></h1>
+    </div>
+</section>
+
+<section class="homepage-content">
+    <?php echo get_the_content(); ?>
+</section>
+
+<?php 
+get_footer();
+?>
